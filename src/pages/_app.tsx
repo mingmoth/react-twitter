2b88ee469386ebd7dbef457100d62ee280b51736
mingmoth <mingmoth@gmail.com>
2023-10-01 17:19:32 +0800

    feat: import SideNav into app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 // Next component
 import Head from "next/head";
+// components
+import SideNav from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,7 +21,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="description" content="This is a Twitter Clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto flex">
+      <div className="container mx-auto flex items-start">
+        <SideNav />
         <div className="min-h-screen flex-grow border-x">
           <Component {...pageProps} />
         </div>
