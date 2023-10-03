17eef70b64154b6d68604276fb4d39aba0af1402
mingmoth <mingmoth@gmail.com>
2023-10-03 22:30:06 +0800

    feat: add infiniteFeed input & query logic

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
@@ -16,5 +17,46 @@ export const tweetRouter = createTRPCRouter({
         }
       })
       return tweet
+    }),
+
+  infiniteFeed: publicProcedure
+    .input(z.object({
+      limit: z.number().optional(),
+      cursor: z.object({
+        id: z.string(),
+        createdAt: z.date()
+      }).optional()
+    }))
+    .query(async({
+      input: { limit = 10, cursor }, ctx
+    }) => {
+      const currentUserId = ctx.session?.user.id
+
+      const tweets = await ctx.db.tweet.findMany({
+        take: limit + 1,
+        cursor: cursor ? { createdAt_id: cursor } : undefined,
+        orderBy: [
+          { createdAt: "desc"},
+          { id: "desc" },
+        ],
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          _count: {
+            select: { likes: true }
+          },
+          likes: currentUserId == null
+            ? false
+            : { where: { userId: currentUserId }},
+          user: {
+            select: {
+              name: true,
+              id: true,
+              image: true,
+            }
+          }
+        }
+      })
     })
 });
