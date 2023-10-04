43032ab43d647cd7955e206acc73508db85788ec
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2023-10-04 17:28:59 +0800

    [Feat] Add Tweet Feed server logic (#6)
    * feat: give tweet schema an unique id by binding createAt time & id
    
    * feat: add infiniteFeed input & query logic
    
    * feat: create nextCursor for next tweets fetching & return tweets api response

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
@@ -16,5 +17,71 @@ export const tweetRouter = createTRPCRouter({
         }
       })
       return tweet
+    }),
+
+  infiniteFeed: publicProcedure
+    .input(z.object({
+      limit: z.number().optional(),
+      cursor: z.object({
+        id: z.string(),
+        createdAt: z.date()
+      }).optional()
+    }))
+    .query(async({
+      input: { limit = 10, cursor }, ctx
+    }) => {
+      const currentUserId = ctx.session?.user.id
+
+      const tweets = await ctx.db.tweet.findMany({
+        take: limit + 1,
+        cursor: cursor ? { createdAt_id: cursor } : undefined,
+        orderBy: [
+          { createdAt: "desc"},
+          { id: "desc" },
+        ],
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          _count: {
+            select: { likes: true }
+          },
+          likes: currentUserId == null
+            ? false
+            : { where: { userId: currentUserId }},
+          user: {
+            select: {
+              name: true,
+              id: true,
+              image: true,
+            }
+          }
+        }
+      })
+
+      let nextCursor: typeof cursor | undefined
+      if(tweets.length > limit) {
+        const lastTweet = tweets.pop()
+        if(lastTweet != null) {
+          nextCursor = {
+            id: lastTweet?.id,
+            createdAt: lastTweet?.createdAt,
+          }
+        }
+      }
+
+      return {
+        tweets: tweets.map(tweet => {
+          return {
+            id: tweet.id,
+            content: tweet.content,
+            createdAt: tweet.createdAt,
+            likeCount: tweet._count.likes,
+            likedByMe: tweet.likes?.length > 0,
+            user: tweet.user,
+          }
+        }),
+        nextCursor
+      }
     })
 });
