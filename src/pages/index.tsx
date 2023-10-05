e3e5e2248282b4f3d41499b06c911a5a17981bf7
mingmoth <mingmoth@gmail.com>
2023-10-05 19:03:22 +0800

    feat: using HomeTweetsSection in Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
 import { type NextPage } from "next";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
-// api
-import { api } from "~/utils/api";
 // components
-import InfiniteTweetList from "~/components/InfiniteTweetList";
 import NewTweetForm from "~/components/NewTweetForm";
-
-const TABS = ["Recent", "Following"] as const;
+import HomeTweetsSection from "~/components/home/HomeTweetsSection";
 
 const Home: NextPage = () => {
-  const session = useSession();
-
-  const [selectTab, setSelectedTab] = useState<(typeof TABS)[number]>("Recent");
 
   return (
     <>
@@ -20,46 +11,9 @@ const Home: NextPage = () => {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
-      {session.status === "authenticated" && (
-        <div
-          className="w-full flex border-b border-b-gray-200"
-        >
-          {TABS.map((tab) => {
-            return (
-              <button
-                className={`px-4 py-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${tab === selectTab
-                    ? "border-b-4 border-b-blue-500 font-bold"
-                    : ""
-                  }`}
-                key={tab}
-                onClick={() => setSelectedTab(tab)}
-              >
-                {tab}
-              </button>
-            );
-          })}
-        </div>
-      )}
-      <RecentTweets />
+      <HomeTweetsSection />
     </>
   );
 };
-
-function RecentTweets() {
-  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
-    {},
-    { getNextPageParam: (lastPage) => lastPage.nextCursor },
-  );
-
-  return (
-    <InfiniteTweetList
-      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
-      isError={tweets.isError}
-      isLoading={tweets.isLoading}
-      hasMore={tweets.hasNextPage || false}
-      fetchNewTweets={tweets.fetchNextPage}
-    />
-  );
-}
 
 export default Home;
