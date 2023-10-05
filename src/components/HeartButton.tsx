59f16e4c032ae0d5726089d69d0404cbea9d28f1
mingmoth <mingmoth@gmail.com>
2023-10-05 14:40:39 +0800

    feat: add HoverEffect style component for HeartButton

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -1,5 +1,8 @@
 import { useSession } from "next-auth/react"
+// react-icons
 import { VscHeart, VscHeartFilled } from "react-icons/vsc"
+// components
+import HoverEffect from "./hover/HoverEffect"
 
 type HeartButtonProps = {
     likeCount: number
@@ -23,21 +26,23 @@ export default function HeartButton({
     }
 
     return (
-        <button>
+        <button className="w-fit">
             <div
-                className={`group flex items-center gap-1 self-start transition-colors duration-200 ${
+                className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
                     likedByMe
                         ? "text-red-500"
                         : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
                 }`}
             >
-                <HeartIcon
-                    className={`transition-colors duration-200 ${
-                        likedByMe
-                            ? "fill-red-500"
-                            : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
-                    }`}
-                />
+                <HoverEffect red>
+                    <HeartIcon
+                        className={`transition-colors duration-200 ${
+                            likedByMe
+                                ? "fill-red-500"
+                                : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+                        }`}
+                    />
+                </HoverEffect>
                 <span>{likeCount}</span>
             </div>
         </button>
