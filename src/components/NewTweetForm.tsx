0d999947770bc864a1e5561c92243f6fb72e629e
mingmoth <mingmoth@gmail.com>
2023-10-05 16:31:41 +0800

    feat: dynamic displaying after tweeting using TRPC setInfiniteData

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -31,13 +31,47 @@ function TweetForm() {
         textAreaRef.current = textArea
     }, [])
 
+    const trpcUtils = api.useContext()
+
     useLayoutEffect(() => {
         if(textAreaRef.current === null) return
         updateTextAreaSize(textAreaRef.current)
     }, [inputValue])
 
     const createTweet = api.tweet.create.useMutation({
-        onSuccess: () => setInputValue("")
+        onSuccess: (newTweet) => {
+            setInputValue("")
+
+            if(session.status !== 'authenticated') return
+
+            trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+                if(oldData == null || oldData.pages[0] == null) return
+
+                const newTweetFormat = {
+                    ...newTweet,
+                    likeCount: 0,
+                    likedByMe: false,
+                    user: {
+                        id: session.data?.user.id,
+                        image: session.data?.user.image || null,
+                        name: session.data?.user.name || null,
+                    }
+                }
+
+                return {
+                    ...oldData,
+                    pages: [
+                        {
+                            ...oldData.pages[0],
+                            tweets: [
+                                newTweetFormat, ... oldData.pages[0].tweets
+                            ]
+                        },
+                        ...oldData.pages.slice(1)
+                    ]
+                }
+            })
+        }
     })
 
     if(session.status !== 'authenticated') return null;
