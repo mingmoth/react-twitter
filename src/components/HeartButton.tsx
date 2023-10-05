425921501c82e0f29e93193f1526df3ea1c09888
mingmoth <mingmoth@gmail.com>
2023-10-05 14:23:41 +0800

    feat: add authenticated HeartButton style

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -24,8 +24,20 @@ export default function HeartButton({
 
     return (
         <button>
-            <div className="mb-1 mt-1 flex items-center gap-1 self-start text-gray-500">
-                <HeartIcon />
+            <div
+                className={`group flex items-center gap-1 self-start transition-colors duration-200 ${
+                    likedByMe
+                        ? "text-red-500"
+                        : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+                }`}
+            >
+                <HeartIcon
+                    className={`transition-colors duration-200 ${
+                        likedByMe
+                            ? "fill-red-500"
+                            : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+                    }`}
+                />
                 <span>{likeCount}</span>
             </div>
         </button>
