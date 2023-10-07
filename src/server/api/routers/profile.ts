ccdb19e33daa07087e2c3fd26c4eee5236543acf
mingmoth <mingmoth@gmail.com>
2023-10-07 17:39:09 +0800

    feat: add toggleFollow profile api

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import {
   createTRPCRouter,
+  protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 
@@ -45,6 +46,57 @@ export const profileRouter = createTRPCRouter({
                 followsCount: profile._count.follows,
                 tweetsCount: profile._count.tweets,
                 isFollowing: profile.followers.length > 0,
+            }
+        }),
+
+    toggleFollow: protectedProcedure
+        .input(
+            z.object({
+                userId: z.string()
+            })
+        )
+        .mutation(async({
+            ctx,
+            input: { userId },
+        }) => {
+            const currentUserId = ctx.session.user.id
+            if(currentUserId === userId) return
+
+            const existingFollow = await ctx.db.user.findFirst({
+                where: {
+                    id: userId,
+                    followers: {
+                        some: { id: currentUserId }
+                    }
+                }
+            })
+
+            let addedFollow
+
+            if(existingFollow == null) {
+                await ctx.db.user.update({
+                    where: { id: userId },
+                    data: {
+                        followers: {
+                            connect: { id: currentUserId }
+                        }
+                    }
+                })
+                addedFollow = true
+            } else {
+                await ctx.db.user.update({
+                    where: { id: userId },
+                    data: {
+                        followers: {
+                            disconnect: { id: currentUserId }
+                        }
+                    }
+                })
+                addedFollow = false
+            }
+
+            return {
+                addedFollow
             }
         })
 });
